baf027f6aea656ab409c364aff4574cb1811a456
Cloonie <imark.liew@gmail.com>
2022-05-24 01:37:26 +0800

    include header

--- libft.h
+++ libft.h
@@ -19,17 +19,19 @@
 # include <strings.h>
 
 int	ft_atoi(char *str);
-void	ft_bzero(void *s, size_t n);
 int	ft_isalnum(int c);
 int	ft_isalpha(char c);
 int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memset(void *b, int c, size_t len);
 int	ft_strlen(char *str);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+
+void	ft_bzero(void *s, size_t n);
+
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
